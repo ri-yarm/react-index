1ebad8d7f41816bc4cac65c89f4c98a2c5698ee4
Rinat Yarmuhametov <thogusus@gmail.com>
2023-06-10 20:06:41 +0500

    done swiper photo on main page

--- src/components/Product/index.tsx
+++ src/components/Product/index.tsx
@@ -1,8 +1,16 @@
 import { Link } from 'react-router-dom';
+import Slider from 'react-slick';
 
 import { formatDate } from '../../utils/date';
 
 import './Product.less';
+
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+import photo1 from '../../assets/photoForSlider1.jpg';
+import photo2 from '../../assets/photoForSlider2.jpg';
+import photo3 from '../../assets/photoForSlider3.jpg';
 
 type ProductProps = {
   id: string;
@@ -21,18 +29,41 @@ const Product: React.FC<ProductProps> = ({
   price,
   title,
   address,
-  about,
   createdAt,
   image,
 }) => {
-  
+  const settings = {
+    dots: true,
+    fade: true,
+    infinite: true,
+    // добавляем ленивой загрузки для оптимизации
+    lazyLoad: true,
+    speed: 500,
+    arrows: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    draggable: true,
+  };
 
   return (
     <article className="product">
       <div className="product__wrapper">
         {seen && <span className="product__seen">Просмотрено</span>}
         <Link to={`items/:${id}`}>
-          <img className="product__img" src={image} alt="" />
+          <Slider {...settings}>
+            <div>
+              <img className="product__img" src={image} alt={` ${title}.`} />
+            </div>
+            <div>
+              <img className="product__img" src={photo1} alt={` ${title}.`} />
+            </div>
+            <div>
+              <img className="product__img" src={photo2} alt={` ${title}.`} />
+            </div>
+            <div>
+              <img className="product__img" src={photo3} alt={` ${title}.`} />
+            </div>
+          </Slider>
         </Link>
         <div className="product__info">
           <div className="product__header">
